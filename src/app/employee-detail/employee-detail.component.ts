1d463211ae820a6b5b5241b83678748a9e695f16
Marcelo Poblete <marcelopoblette@gmail.com>
2020-03-18 17:59:26 -0300

    Error Handling

--- src/app/employee-detail/employee-detail.component.ts
+++ src/app/employee-detail/employee-detail.component.ts
@@ -8,13 +8,16 @@ import { EmployeeService } from '../employee.service';
 })
 export class EmployeeDetailComponent implements OnInit {
 
-  public employees = []
+  public employees = [];
+  public errorMsg;
 
   constructor(private _employeeService: EmployeeService) { }
 
   ngOnInit() {
     this._employeeService.getEmployees()
-        .subscribe(data => this.employees = data);
+        .subscribe(data => this.employees = data,
+          error => this.errorMsg = error);
+
   }
 
 }
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,20 +1,41 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { IEmployee } from './employee';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs'; // programacion reactiva : mapp, errror, 
+import { catchError } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class EmployeeService {
 
   private _url: string = "/assets/data/employees.json";
 
   constructor(private http: HttpClient) { }
+
   getEmployees(): Observable<IEmployee[]>{
 
-    return this.http.get<IEmployee[]>(this._url);
-     
-   
+    return this.http.get<IEmployee[]>(this._url).pipe(catchError(this.errorHandler));
   }
+  
+  errorHandler(error: HttpErrorResponse){
+    return Observable.throw(error.message || "Server Error");
+  }
+
+
+  
+  // private _handleError(error: HttpErrorResponse) {
+  //   if (error.error instanceof ErrorEvent) {
+  //     // A client-side or network error occurred. Handle it accordingly.
+  //     console.error('Error:', error.error.message);
+  //   } else {
+  //     // The backend returned an unsuccessful response code.
+  //     // The response body may contain clues as to what went wrong,
+  //     console.error(
+  //       `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+  //     );
+  //   }
+  //   // return an observable with a user-facing error message
+  //   return throwError('Ups no se puede acceder, por favor intente más tarde');
+  // }
 }
+
+
